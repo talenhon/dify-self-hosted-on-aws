53e557f7572e11df826074751a5aea783340dbcf
Black Currant <39578608+talenhon@users.noreply.github.com>
2025-10-28 21:17:55 +0800

    Update AWS region and image tags in cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -6,18 +6,21 @@ import { UsEast1Stack } from '../lib/us-east-1-stack';
 import { EnvironmentProps } from '../lib/environment-props';
 
 export const props: EnvironmentProps = {
-  awsRegion: 'us-west-2',
+  awsRegion: 'ap-east-1',
   awsAccount: process.env.CDK_DEFAULT_ACCOUNT!,
   // Set Dify version
-  difyImageTag: '1.9.1',
+  difyImageTag: '1.9.2',
   // Set plugin-daemon version to stable release
-  difyPluginDaemonImageTag: '0.3.1-local',
+  difyPluginDaemonImageTag: '0.3.3-local',
 
   // uncomment the below options for less expensive configuration:
   // isRedisMultiAz: false,
   // useNatInstance: true,
   // enableAuroraScalesToZero: true,
   // useFargateSpot: true,
+
+  domainName:visiostorm.com
+  
 
   // Please see EnvironmentProps in lib/environment-props.ts for all the available properties
 };
